d595685a5308d2ddd960b142eca256b4649ff295
DeepaSakthivel <deepasakthivel115@gmail.com>
2021-04-03 09:04:52 +0530

    edit

--- salaryfile/settings.py
+++ salaryfile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*j+!a@&latz4hnz^1x&hgz3x2jhp+ya3decho8q*9w&l=*om4o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['18.220.178.226','localhost']
+ALLOWED_HOSTS = ['3.142.225.158','localhost']
 
 
 # Application definition
